65c1f15f925364f3abdeb4209766a57b6bd7fff1
bhuvan2018 <bhuvanshetty2018@gmail.com>
2025-04-07 13:21:48 +0530

    commit

--- todo_app/api/login.php
+++ todo_app/api/login.php
@@ -3,7 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// Handle CORS preflight request
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
     http_response_code(200);
     exit();
